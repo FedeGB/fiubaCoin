c2267e5be25ce712627ea5e6cf8b1097dc30adc8
Federico Garcia Brion <fedeagb@gmail.com>
2018-06-24 23:17:43 -0300

    Working fiubaCoin. I have to solve the problem that the accounts have unsigned ints for balances.

--- fiubaCoin.sol
+++ fiubaCoin.sol
@@ -1,37 +1,6 @@
 pragma solidity ^0.4.21;
 
 contract fiubaCoin {
-    // The keyword "public" makes those variables
-    // readable from outside.
-    address public minter;
-    mapping (address => uint) public balances;
-
-    // Events allow light clients to react on
-    // changes efficiently.
-    event Sent(address from, address to, uint amount);
-
-    // This is the constructor whose code is
-    // run only when the contract is created.
-    constructor() public {
-        minter = msg.sender;
-        balances[msg.sender] = 10000;
-    }
-
-    function mint(address receiver, uint amount) public {
-        if (msg.sender != minter) return;
-        balances[receiver] += amount;
-    }
-
-    function send(address receiver, uint amount) public {
-        // if (balances[msg.sender] < amount) return;
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
-        emit Sent(msg.sender, receiver, amount);
-    }
-}
-
-
-contract RockPaperScissors {
     
     struct Person {
         uint64 cuil;
@@ -57,11 +26,27 @@ contract RockPaperScissors {
     Apuesta private apuesta01;
     Apuesta private apuesta02;
 
+    mapping (address => uint) public balances;
 
-    constructor() public {
+    event Sent(address from, address to, uint amount);
+
+    constructor() public payable {
         creator = msg.sender;
-        createdAt = now; 
+        createdAt = now;
         // Da el timestamp de la blockchain.. lo cual es dudoso, pero no hay forma de obtener un time real..
+        balances[msg.sender] = 10000;
+    }
+
+    function mint(address receiver, uint amount) public {
+        if (msg.sender != creator) return;
+        balances[receiver] += amount;
+    }
+
+    function transfer(address sender, address receiver, uint amount) internal {
+        // if (balances[msg.sender] < amount) return; Se aceptan numeros negativos en los balances
+        balances[sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(sender, receiver, amount);
     }
 
     function setApostador01(uint64 cuil01, string name01, address blockDir01) public {
@@ -75,7 +60,7 @@ contract RockPaperScissors {
         gambler02 = Person({cuil: cuil02, nombre: name02, blockDir: blockDir02});
     }
 
-    function apostar(uint money, gambleOption election) public {
+    function apostar(uint money, gambleOption election) public payable {
         require(election == gambleOption.PIEDRA || election == gambleOption.PAPEL || election == gambleOption.TIJERA, "La eleccion no es valida");
         Apuesta memory entrante = Apuesta({eleccion: election, dinero: money});
         if(msg.sender == gambler01.blockDir) {
@@ -85,28 +70,7 @@ contract RockPaperScissors {
         } else {
             revert("Solo los que forman parte de la apuesta pueden apostar");
         }
-
-        if(apuesta01.dinero > 0 && apuesta02.dinero > 0) {
-            address sender;
-            address receiver;
-            uint cash;
-            if(resolveGame(apuesta01.eleccion, apuesta02.eleccion)) {
-                sender = gambler02.blockDir;
-                receiver = gambler01.blockDir;
-            } else {
-                sender = gambler01.blockDir;
-                receiver = gambler02.blockDir;
-            }
-            if(apuesta01.dinero < apuesta02.dinero) {
-                cash = apuesta01.dinero;
-            } else {
-                cash = apuesta02.dinero;
-            }
-            fiubaCoin token = fiubaCoin(sender);
-            token.send(receiver, cash);
-            
-                
-        }
+        resolveGamble();
     }
 
     function resolveGame(gambleOption first, gambleOption second) internal pure returns(bool) {
@@ -128,6 +92,35 @@ contract RockPaperScissors {
         if(first == gambleOption.TIJERA && second == gambleOption.PIEDRA) {
             return false;
         }
+    }
+    
+    function resolveGamble() internal {
+        if(apuesta01.dinero > 0 && apuesta02.dinero > 0) {
+            address sender;
+            address receiver;
+            uint cash;
+            if(resolveGame(apuesta01.eleccion, apuesta02.eleccion)) {
+                sender = gambler02.blockDir;
+                receiver = gambler01.blockDir;
+            } else {
+                sender = gambler01.blockDir;
+                receiver = gambler02.blockDir;
+            }
+            if(apuesta01.dinero < apuesta02.dinero) {
+                cash = apuesta01.dinero;
+            } else {
+                cash = apuesta02.dinero;
+            }
+            transfer(sender, receiver, cash);
+            clearGamble();
+        }
+    }
+
+    function clearGamble() internal {
+        delete gambler01;
+        delete gambler02;
+        delete apuesta01;
+        delete apuesta02;
     }
 
 }
